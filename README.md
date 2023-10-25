5348ba1bb239b674212f2e7804d6431f4b5d3fbb
tekkahammadxo <tarekmagdy193@gmail.com>
2023-10-25 16:38:42 +0300

    initial
    chat app task using socket io

---
+++ README.md
@@ -0,0 +1,3 @@
+A web chat rooms using socket io and laravel using vue js
+
+Realtime chat app using Laravel, VueJS, Redis, Laravel Echo, SocketIO
---
+++ resources/js/components/Footer.vue
@@ -0,0 +1,35 @@
+<template>
+  <footer id="footer" class="w-100">
+    <div>
+        This task is created based on request from Mr Rizik for hiring
+    </div>
+    
+</footer>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+footer {
+  background: #1c5986;
+  color: white;
+  text-align: center;
+  position: absolute;
+  bottom: 0;
+  font-size: 13px;
+  font-weight: 300;
+}
+
+@media only screen and (max-width: 768px) {
+  footer {
+    position: initial;
+  }
+}
+
+footer a {
+  color: #00ffe9;
+}
+</style>
